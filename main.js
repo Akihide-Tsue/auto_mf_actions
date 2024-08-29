5c5295bfd79a4086007c83cff834f591dc4f60be
Akihide-Tsue <akihide.tsue@gmail.com>
2024-08-29 09:55:44 +0900

    不要箇所削除

--- main.js
+++ main.js
@@ -6,71 +6,7 @@ const puppeteer = require('puppeteer');
 const { setTimeout } = require("timers/promises");
 
 (async () => {
-  // 環境変数から認証情報を取得
-  // const clientId = process.env.GOOGLE_OAUTH_CLIENT_ID
-  // const clientSecret = process.env.GOOGLE_OAUTH_CLIENT_SECRET
-  // const refreshToken = process.env.GOOGLE_OAUTH_REFRESH_TOKEN
 
-  // 認証処理
-  // const getGoogleOAuth = async () => {
-  //   const googleOAuth = new google.auth.OAuth2(
-  //     clientId,
-  //     clientSecret,
-  //     'http://localhost',
-  //   )
-
-  //   // 毎回のリクエスト時に新しいアクセストークンを取得
-  //   googleOAuth.setCredentials({
-  //     refresh_token: refreshToken,
-  //   })
-
-  //   try {
-  //     const accessTokenResponse = await googleOAuth.getAccessToken()
-  //     const accessToken = accessTokenResponse.token
-
-  //     if (!accessToken)
-  //       throw new Error('有効なアクセストークンを取得できませんでした')
-
-  //     googleOAuth.setCredentials({
-  //       access_token: accessToken,
-  //     })
-
-  //     return googleOAuth
-  //   } catch (err) {
-  //     console.log('エラーの中身', err)
-  //     throw new Error('アクセストークン取得時にエラーが発生しました')
-  //   }
-  // }
-
-  // カレンダーからイベントを取得
-  // const getEventListFromGoogleCalendar = async (
-  //   timeMin,
-  //   timeMax,
-  // ) => {
-  //   try {
-  //     const googleOAuth = await getGoogleOAuth()
-  //     const calendar = google.calendar({ version: 'v3', auth: googleOAuth })
-
-  //     const res = await calendar.events.list({
-  //       calendarId: 'primary',
-  //       timeMin,
-  //       timeMax,
-  //       timeZone: 'Asia/Tokyo',
-  //     })
-
-  //     console.log("res.data.items", res.data.items)
-
-  //     const holidaySchedule = res.data.items.filter(item => ['打刻なし'].some(keyword => item.summary.includes(keyword)))
-
-  //     console.log("打刻なしあり", holidaySchedule.length > 0)
-
-  //     if (holidaySchedule.length > 0) return true
-  //     else return false
-
-  //   } catch (err) {
-  //     console.log('カレンダーからイベント取得時にエラーが発生しました', err)
-  //   }
-  // }
 
   const mfPuppeteer = async (currentHour) => {
     let browser // browser変数を定義
@@ -92,7 +28,6 @@ const { setTimeout } = require("timers/promises");
       const page = await browser.newPage()
       await page.setUserAgent('Mozilla/5.0 (Macintosh Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36')
       await page.goto('https://attendance.moneyforward.com/employee_session/new', { waitUntil: ['load', 'networkidle2'] })
-      // await setTimeout(Math.floor(Math.random() * 600000))//打刻時間をバラけさせる
 
       await page.click('a[class="attendance-button-mfid attendance-button-link attendance-button-size-wide"]')
       console.log('ページ遷移')
@@ -168,17 +103,6 @@ const { setTimeout } = require("timers/promises");
     now.setHours(now.getHours() + 9); // 9時間足す
     console.log("日本時刻", now)
 
-    // const startOfDay = new Date(now.setHours(0, 0, 0, 0)).toISOString();
-    // const endOfDay = new Date(now.setHours(23, 59, 59, 999)).toISOString()
-    // console.log("startOfDay", startOfDay,)
-    // console.log("endOfDay", endOfDay)
-
-    // const isHoliday = await getEventListFromGoogleCalendar(startOfDay, endOfDay)
-
-    // if (isHoliday) {
-    //   return
-    // } else {
-    // }
     await mfPuppeteer(currentHour)
 
   } catch (error) {
